b613a80fc6098244b12d5ea406cae49ac24e4b72
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-18 22:17:05 -0500

    create employee list

---
+++ employee-directory/src/utils/Employees.js
@@ -0,0 +1,94 @@
+const employees = [
+  {
+    id: 1,
+    fname: 'Hermione',
+    lname: 'Granger',
+    position: 'Information Clerk'
+  },
+  {
+    id: 2,
+    fname: 'Harry',
+    lname: 'Potter',
+    position: 'Information Clerk'
+  },
+  {
+    id: 3,
+    fname: 'Ron',
+    lname: 'Weasley',
+    position: 'Information Clerk'
+  },
+  {
+    id: 4,
+    fname: 'Lord',
+    lname: 'Voldemort',
+    position: 'Records Management Analysis'
+  },
+  {
+    id: 5,
+    fname: 'Draco',
+    lname: 'Malfoy',
+    position: 'Records Management Analysis'
+  },
+  {
+    id: 6,
+    fname: 'Severus',
+    lname: 'Snape',
+    position: 'Records Management Analysis'
+  },
+  {
+    id: 7,
+    fname: 'Rubeus',
+    lname: 'Hagrid',
+    position: 'Data Entry'
+  },
+  {
+    id: 8,
+    fname: 'Minerva',
+    lname: 'McGonagall',
+    position: 'Data Entry'
+  },
+  {
+    id: 9,
+    fname: 'Dolores',
+    lname: 'Umbridge',
+    position: 'Data Entry'
+  },
+  {
+    id: 10,
+    fname: 'James',
+    lname: 'Potter',
+    position: 'Support Specialist'
+  },
+  {
+    id: 11,
+    fname: 'Remus',
+    lname: 'Lupin',
+    position: 'Support Specialist'
+  },
+  {
+    id: 12,
+    fname: 'Sirius',
+    lname: 'Black',
+    position: 'Support Specialist'
+  },
+  {
+    id: 13,
+    fname: 'Ginny',
+    lname: 'Weasley',
+    position: 'Support Assistant'
+  },
+  {
+    id: 14,
+    fname: 'Neville',
+    lname: 'Longbottom',
+    position: 'Support Assistant'
+  },
+  {
+    id: 15,
+    fname: 'Luna',
+    lname: 'Lovegood',
+    position: 'Support Assistant'
+  }
+];
+
+export default employees;
