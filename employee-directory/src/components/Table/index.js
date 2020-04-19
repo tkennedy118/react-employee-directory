8d120d2575b1926e6f5fd7fc0211f323fcbf6eb4
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-19 10:56:04 -0500

    create components for col, row, container, and table

---
+++ employee-directory/src/components/Table/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Table(props) {
+  console.log('PROPS: ', props);
+  const employeeList = () => {
+    return props.employees.map(employee => (
+      <tr key={employee.id}>
+        <th scope='row'>{employee.id}</th>
+        <td>{employee.fname}</td>
+        <td>{employee.lname}</td>
+        <td>{employee.position}</td>
+      </tr>
+    ));
+  }
+
+  return (
+    <table className={`table table-${props.theme}`}>
+      <thead>
+        <tr>
+          <th scope='col'>ID</th>
+          <th scope='col'>First</th>
+          <th scope='col'>Last</th>
+          <th scope='col'>Position</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employeeList()}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- employee-directory/src/pages/Home.js
+++ employee-directory/src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import employees from '../utils/employees';
+import Container from '../components/Container/index';
+import Row from '../components/Row/index';
+import Col from '../components/Col/index';
+import Table from '../components/Table/index';
 
 function Home() {
+  console.log('EMPLOYEES: ', employees);
+
   return(
-    <div>
-      <h2>This is the home page</h2>
-    </div>
-  );
+    <Container>
+      <Row>
+        <Col size='md-12'>
+          <Table employees={employees} theme='light'></Table>
+        </Col>
+      </Row>
+    </Container>
+  ); 
 }
 
 export default Home;
